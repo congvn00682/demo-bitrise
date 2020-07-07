a731a68338c194b0c5d0fe5e3c83a929ec656096
Vu Ngoc Cong <congvn00682@gmail.com>
2020-07-07 11:37:56 +0700

    init project

---
+++ demo-bitrise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demo-bitrise
+//
+//  Created by Vu Ngoc Cong on 7/7/20.
+//  Copyright © 2020 Vu Ngoc Cong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
